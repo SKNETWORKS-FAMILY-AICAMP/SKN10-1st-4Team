1b964ee0075c56833d61b799215bfeec72dc5aaa
shinminju <sinminju73@gmail.com>
2025-01-22 17:37:09 +0900

    제네시스 차량 구매 FAQ

--- pages/3_주요_3개_기업_차량_구매_FAQ.py
+++ pages/3_주요_3개_기업_차량_구매_FAQ.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import json
 
 st.title("❓ 주요 3개 기업 차량 구매 FAQ")
 st.divider()
@@ -13,3 +14,13 @@ with tab2:
 
 with tab3:
     st.write("제네시스 차량 구매 FAQ")
+    
+    file_path = 'genesis_faq.json'
+    with open(file_path, 'r', encoding='utf-8') as file:
+        faq_data = json.load(file)
+
+    for item in faq_data:
+        question = item.get("question", "질문 없음")
+        answer = item.get("answer", "답변 없음")
+        with st.expander(f"❓ {question}"):
+            st.write(answer)

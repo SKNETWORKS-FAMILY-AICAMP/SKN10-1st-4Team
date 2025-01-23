003616010516aaafd4f95fa89e5959ac35945fbd
ine <jwamin0904@gmail.com>
2025-01-23 15:14:16 +0900

    modify path jenesis_faq

--- pages/3_주요_3개_기업_차량_구매_FAQ.py
+++ pages/3_주요_3개_기업_차량_구매_FAQ.py
@@ -146,11 +146,6 @@ with tab2:
 with tab3:
     st.write("제네시스 차량 구매 FAQ")
     
-<<<<<<< HEAD
-    file_path = 'data/genesis_faq.json' # 경로설정
-    with open(file_path, 'r', encoding='utf-8') as file:
-        faq_data = json.load(file)
-
     # JSON 파일 로드
     file_path = 'data\genesis_faq.json'
     try:
@@ -161,38 +156,14 @@ with tab3:
         faq_data = []
 
     # 세션 상태 초기화
-    if 'current_page' not in st.session_state:
-        st.session_state.current_page = 0
-=======
-    # JSON 파일 로드
-    file_path = 'genesis_faq.json'
-    try:
-        with open(file_path, 'r', encoding='utf-8') as file:
-            faq_data = json.load(file)
-    except Exception as e:
-        st.error(f"FAQ 데이터를 로드하는 중 오류 발생: {e}")
-        faq_data = []
-
-    # 세션 상태 초기화
     if 'page' not in st.session_state:
         st.session_state.page = 1
->>>>>>> origin/inho
 
     # FAQ 데이터 확인 및 예외 처리
     if len(faq_data) == 0:
         st.write("FAQ 데이터가 없습니다.")
         st.stop()
 
-<<<<<<< HEAD
-    # 한 페이지에 표시할 FAQ 수와 페이지 계산
-    faq_per_page = 5
-    total_pages = (len(faq_data) - 1) // faq_per_page + 1
-
-    # 현재 페이지에 해당하는 FAQ 가져오기
-    start_index = st.session_state.current_page * faq_per_page
-    end_index = start_index + faq_per_page
-    current_faqs = faq_data[start_index:end_index]
-=======
     # 검색 기능
     search_query = st.text_input("검색어를 입력하세요:")
     if search_query:
@@ -214,7 +185,6 @@ with tab3:
     start_index = (page - 1) * faq_per_page
     end_index = start_index + faq_per_page
     current_faqs = filtered_data[start_index:end_index]
->>>>>>> origin/inho
 
     # 현재 페이지의 FAQ 출력
     for item in current_faqs:
@@ -222,8 +192,6 @@ with tab3:
         answer = item.get("answer", "답변 없음")
         with st.expander(f"❓ {question}"):
             st.write(answer)
-<<<<<<< HEAD
-=======
     
     # 이전, 다음 버튼을 양쪽 끝에 배치하고 가운데에 현재 페이지 표시
     button_container = st.container()
@@ -238,4 +206,3 @@ with tab3:
         with col3:
             if page < total_pages and st.button("다음", key="next_page"):
                 st.session_state.page = page + 1
->>>>>>> origin/inho
